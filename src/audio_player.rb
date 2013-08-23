c8a33c86fa8345f1f8f2c856d5949e26333716a2
Anz_MSI <pjc0247@naver.com>
2013-08-23 16:14:58 +0900

    add audio module

---
+++ src/audio_player.rb
@@ -0,0 +1,39 @@
+# audio_player.rb
+#
+#   written by pjc0247
+#
+
+require 'win32api'
+
+load 'gen_random.rb'
+
+class AudioPlayer
+	@@mciSendString = Win32API.new("winmm", "mciSendString", "PPLL", "L")
+	
+	def initialize(file)
+		@instance_name = "rb_" + generate_random_string(4)
+
+		send "open #{file} alias #{@instance_name}"
+
+		self.volume = 500
+	end
+
+	def send(str)
+		@@mciSendString.call str, "", 0, 0
+	end
+
+	def play
+		send "play #{@instance_name}"
+	end
+	def stop
+		send "stop #{@instance_name}"
+	end
+
+	def volume=(v)
+		send "setaudio #{@instance_name} volume to #{v}"
+		@volume = v 
+	end
+	def volume
+		@volume
+	end
+end
